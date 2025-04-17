a1d0c899c710872b5e37e18bf77d8b675e477bab
FrozenLemonTee <1115306170@qq.com>
2025-04-17 23:18:23 +0800

    feat: Add EVAL (todo)

--- main.cpp
+++ main.cpp
@@ -6,12 +6,35 @@
 #include "reader.h"
 #include "types.h"
 #include "printer.h"
+#include "env.h"
+
 
 MalType* READ(std::string input){
     return Reader::read_str(std::move(input));
 }
 
-MalType* EVAL(MalType* input) {
+MalType* EVAL(MalType* input, Env& env) {
+    if (auto sym = dynamic_cast<MalSymbol*>(input); sym){
+        MalType* opt = env.get(sym->name());
+        if (!opt){
+            throw typeError("Unknown symbol " + std::string("\"") +  sym->name() + std::string("\""));
+        }
+        return opt;
+    }
+
+    if (auto lst = dynamic_cast<MalList*>(input); lst){
+        std::vector<MalType*> eval_args;
+        for (auto& arg: lst->get_elem()){
+            eval_args.emplace_back(EVAL(arg, env));
+        }
+
+        auto fn = dynamic_cast<MalFunction*>(eval_args[0]);
+        if (!fn){
+            throw typeError(eval_args[0]->to_string() + "is not a function");
+        }
+        return fn->apply({eval_args.begin() + 1, eval_args.end()});
+    }
+
     return input;
 }
 
@@ -20,6 +43,8 @@ std::string PRINT(MalType* input) {
 }
 
 int main(){
+    Env env;
+
     while(true){
         std::cout << "user> ";
         std::string input;
@@ -27,7 +52,7 @@ int main(){
             std::cout << std::endl;
             break;
         }try {
-            std::cout << PRINT(EVAL(READ(input))) << std::endl;
+            std::cout << PRINT(EVAL(READ(input), env)) << std::endl;
         }catch(const syntaxError& e) {
             std::cout << e.what() << std::endl;
         }
