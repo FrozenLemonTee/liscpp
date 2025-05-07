a0e9634c61a32c6dbe17d03feb945be7f37f4a32
FrozenLemonTee <1115306170@qq.com>
2025-05-07 23:12:47 +0800

    feat: Add new constructor, find, clone

--- lib/env.cpp
+++ lib/env.cpp
@@ -1,5 +1,6 @@
 #include "env.h"
 #include "builtin.h"
+#include "error.h"
 
 
 void Env::builtin_register() {
@@ -19,8 +20,8 @@ void Env::builtin_register() {
     this->add(">=", new MalFunction(greater_equal));
 }
 
-Env::Env(Env *host, bool is_global) : symbols(), host_env(host) {
-    if (is_global){
+Env::Env(Env *host, bool is_global) : symbols(), host_env(host), global_(is_global) {
+    if (this->global_){
         this->builtin_register();
     }
 }
@@ -45,5 +46,35 @@ void Env::set(const std::string &name, MalType *symbol) {
 Env::~Env() {
     for (auto& [name, symbol]: this->symbols) {
         delete symbol;
+    }
+}
+
+Env* Env::find(const std::string &name) {
+    if (this->symbols.contains(name))
+        return this;
+    if (this->host_env){
+        return this->host_env->find(name);
+    }
+    return nullptr;
+}
+
+Env* Env::clone() const {
+    Env* cloned_env = new Env(this->host_env, this->global_);
+    for (const auto& [name, symbol]: this->symbols){
+        cloned_env->add(name, symbol->clone());
+    }
+    return cloned_env;
+}
+
+Env::Env(Env *host, const std::vector<std::string> &args_list,
+         MalFunction::mal_func_args_list_type &params_list) : Env(host, false) {
+    if (args_list.size() != params_list.size()){
+        throw argInvalidError("expected" + std::to_string(args_list.size()) + " arg(s), given "
+                              + std::to_string(params_list.size()) + "arg(s)");
+    }
+    auto it_args = args_list.begin();
+    auto it_params = params_list.begin();
+    for (;it_args != args_list.end() && it_params != params_list.end(); it_args++, it_params++){
+        this->add(*it_args, *it_params);
     }
 }
